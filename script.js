58e7d2ddf3553949f039d11aff12aec6eed6df01
Hugo Ian <ahugaao@gmail.com>
2021-03-10 13:24:09 -0300

    ajustes nas condicoes

--- script.js
+++ script.js
@@ -29,16 +29,16 @@ function verificaImc() {
         case (total.toFixed(1) < 18.5):
             resultado.innerText = `${nome.value}, seu imc é ${total.toFixed(1)} (Magreza)`
         break
-        case (total.toFixed(1) > 18.5) && (total.toFixed(1) < 24.9):
+        case (total.toFixed(1) >= 18.5) && (total.toFixed(1) <= 24.9):
             resultado.innerText = `${nome.value}, seu imc é ${total.toFixed(1)} (Normal)`
         break
-        case (total.toFixed(1) > 25.0) && (total.toFixed(1) < 29.9):
+        case (total.toFixed(1) >= 25.0) && (total.toFixed(1) <= 29.9):
             resultado.innerText = `${nome.value}, seu imc é ${total.toFixed(1)} (Sobrepeso)`
         break
-        case (total.toFixed(1) > 30.0) && (total.toFixed(1) < 39.9):
+        case (total.toFixed(1) >= 30.0) && (total.toFixed(1) <= 39.9):
             resultado.innerText = `${nome.value}, seu imc é ${total.toFixed(1)} (Obesidade)`
         break
-        case (total.toFixed(1) > 40.0):
+        case (total.toFixed(1) >= 40.0):
             resultado.innerText = `${nome.value}, seu imc é ${total.toFixed(1)} (Obesidade Grave)`
         break
         default:
